4a88ba26e796e3687f3e836f4b6a0abb6ce0c02c
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-11-11 20:39:59 +0200

    testing..

--- ConsoleToWEBApi/ConsoleToWEBApi/Startup.cs
+++ ConsoleToWEBApi/ConsoleToWEBApi/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,14 +16,40 @@ namespace ConsoleToWEBApi
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-          
-            app.UseDeveloperExceptionPage();
-          
+            
+            
+                app.UseDeveloperExceptionPage();
+                Console.WriteLine("dev mode;;");
+
+                app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("hello from use 1 \n");
+                await next();
+            });
+
+             app.Map("/ahab", CustomCode);
+             
+             app.Use(async (context, next) =>
+             {
+                 await context.Response.WriteAsync(" keke \n");
+             });
+            
+           
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+        }
+
+        private void CustomCode(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("hello path middleware");
+                await next();
             });
         }
     }
