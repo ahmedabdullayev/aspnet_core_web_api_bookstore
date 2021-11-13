396f6ab209ab47190cb21468fd0f07dd7c79dd3d
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-11-13 13:30:20 +0200

    Validation for model (Required, minlength, etc)

--- BookStoreApi/Models/BookModel.cs
+++ BookStoreApi/Models/BookModel.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookStoreApi.Models
 {
     public class BookModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please add title property")]
+        [MinLength(3)]
         public string Title { get; set; }
         public string Description { get; set; }
     }
